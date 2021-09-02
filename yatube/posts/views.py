0ff26f672057f2af393146f51d4fda6d05de0239
Dmitry <sdvkam@gmail.com>
2021-09-02 19:26:25 +1200

    Final task of 6 sprint_add4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
